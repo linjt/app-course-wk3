e67562b1e43d339c9193db15e3aa63d536de3b51
J.T.LIN <ruby0963975789@gmail.com>
2022-03-22 00:29:57 +0800

    add screens

---
+++ src/screens/WishListScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const WishListScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Here is WishListScreen.js</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  }
+});
+
+export default WishListScreen;
