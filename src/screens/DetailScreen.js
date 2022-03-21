604d031f4b6e9e67002fb96c938815d34437fc54
J.T.LIN <ruby0963975789@gmail.com>
2022-03-22 00:30:40 +0800

    detail screen

---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { StyleSheet, Pressable, ScrollView, Text, View, Image, Button } from 'react-native';
+
+const DetailScreen = ({ route }) => {
+  const { title, 
+    author,
+    image,
+    star,
+    price,
+    description
+  } = route.params;
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          style={styles.imageStyle}
+          source={{
+            uri: image
+          }}
+        />
+        <View style={styles.cardContainerStyle}>
+          <Text style={{fontWeight:'bold',paddingTop:20,fontSize:30,textAlign:'center'}}> {title} </Text>
+          <Text style={{fontWeight:'600',fontSize:18, textAlign:'center',color:"#666",paddingTop:10}}> {author} </Text>         
+        </View>
+        <View style={styles.starcontainerStyle}>
+          {/* for star */}
+          <Text style={{fontWeight:'600',fontSize:18, textAlign:'center',color:"#131313",paddingTop:10}}> {star}.0 </Text>         
+          <Text style={{fontWeight:'600',fontSize:18, textAlign:'center',color:"#666",paddingTop:10}}> /5.0 </Text>   
+        </View>
+        <Text style={{lineHeight: 18,fontSize:15, textAlign:'center',marginTop:20, lineHeight:25 }}>{'\t'}{description}</Text>
+        <View style={styles.cardContainerStyle}>
+          <Pressable style={styles.button}>
+            <Text style={{color:"#fff",letterSpacing:2,fontWeight:'bold',fontSize: 15}}>BUY NOW FOR ${price}</Text>
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 20,
+    paddingBottom: 20
+  },  
+  starcontainerStyle:{
+    flex: 1,
+    flexDirection:"row",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    height: 320,
+    width: 220
+  },
+  cardContainerStyle: {
+    backgroundColor: '#fff',
+    padding: 10,
+    marginHorizontal: 10,
+    marginTop: 10
+  },
+  button:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#6200EE',
+    width:'100%',
+  }
+});
+
+export default DetailScreen;
